5182f4476eed449c87960c1cc8f3731b5ba1e02c
furkanbalkaya <72027461+furkanbalkaya@users.noreply.github.com>
2022-10-03 12:04:48 +0300

    for_while_loops for_while_loops

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print('hello world!!!')
+for i in range(0,10,2):
+    print(i, end=" ")
+print()
+
+
+
+
+i = 1 
+while i <6:
+    print("*"*i)
+    i = i + 1
+ 
+for i in range(1,6,2):
+    if i %2==0:
+        pass
+    else:
+        print("*" * i)
